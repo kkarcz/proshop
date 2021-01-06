69a499232deba61909b1e8cb4955084e22b3170a
Krzysztof <kzyskarcz@gmail.com>
2021-01-06 19:55:54 +0100

    display products list

---
+++ frontend/src/components/Product.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card } from "react-bootstrap";
+
+interface IProduct {
+    _id: string;
+    name: string;
+    image: string;
+    description: string;
+    brand: string;
+    category: string;
+    price: number;
+    countInStock: number;
+    rating: number;
+    numReviews: number;
+}
+
+interface ProductProps {
+    product: IProduct;
+}
+
+const Product = ({ product: { _id, image, name, rating, numReviews, price } }: ProductProps) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <a href={`/product/${_id}`}>
+                <Card.Img src={image} variant='top' />
+            </a>
+            <Card.Body>
+                <a href={`/product/${_id}`}>
+                    <Card.Title as='div'>
+                        <strong>{name}</strong>
+                    </Card.Title>
+                </a>
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        {rating} from {numReviews} reviews
+                    </div>
+                </Card.Text>
+                <Card.Text as='h3'>${price}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Product;
